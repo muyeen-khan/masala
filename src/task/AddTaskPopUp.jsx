ad33e9e656a5aa8d72cef05db3ae727c26b7d514
muyeen-khan <muyeenkhan80@gmail.com>
2024-01-30 00:18:59 +0600

    add modal bg updated

--- src/task/AddTaskPopUp.jsx
+++ src/task/AddTaskPopUp.jsx
@@ -66,7 +66,8 @@ export default function AddTaskPopUp({ onClose, editable }) {
 
   return (
     <>
-      <div className="bg-black bg-opacity-70 h-full w-full z-10 absolute top-0 left-0"></div>
+      <div className="bg-black bg-opacity-70 min-h-screen w-full z-10 fixed top-0 left-0"></div>
+
       <form className="mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11 z-10 absolute top-1/4 left-1/3">
         <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
           {editable ? "Edit task" : "Add New Task"}
